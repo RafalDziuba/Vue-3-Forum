dd344c72763903ebe4fb3b9d4a729f0e4aa508bd
RafalDziuba <72468641+RafalDziuba@users.noreply.github.com>
2023-09-25 14:32:58 +0200

    Add Pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { createApp } from 'vue';
 import './style.css';
-import App from './App.vue';
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
+import { createPinia } from 'pinia';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+import App from './App.vue';
 
 const vuetify = createVuetify({
   components,
   directives,
 });
 
+const pinia = createPinia(); 
 const app = createApp(App);
-app.mount('#app');
+
 app.use(vuetify);
+app.use(pinia);
+
+app.mount('#app');
