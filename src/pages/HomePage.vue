b2525a211e70f2f63cc3d6fb65d9d67c8c23bd87
RafalDziuba <72468641+RafalDziuba@users.noreply.github.com>
2023-09-28 23:16:10 +0200

    Render posts with user name from threads

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import data from '../data/data.json';
-const threads = data.threads;
-const posts = data.posts;
+
+const threads = ref(data.threads);
+
+const posts = ref(data.posts);
+const fullPostFromId = (postId: string) => {
+  return posts.value.find((p) => p.id === postId);
+};
+
+const users = ref(data.users);
+const fullUserFromPostId = (postId: string) => {
+  return users.value.find((u) => u.id === fullPostFromId(postId)?.userId);
+};
+console.log(posts);
+console.log(threads);
+console.log(users);
 </script>
 
 <template>
   <div>
     <h1>This is HomePage</h1>
     <div v-for="thread in threads" :key="thread.id">
-      <div v-for="post in posts" :key="post.id">
-        <p>{{ post }}</p>
+      <p>
+        <b>{{ thread.title }}</b>
+      </p>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{ fullUserFromPostId(postId)?.name }}</p>
+        <p>{{ fullPostFromId(postId)?.text }}</p>
       </div>
     </div>
   </div>
